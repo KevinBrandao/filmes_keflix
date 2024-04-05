5816222ca16544cbd45ea7d8741b7658060a880d
KevinBrandao <kevenbrandaoms@outlook.com>
2024-04-05 17:00:01 -0300

    finalizando as priemiras 55 paginas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
+import './home.css';
 
 function Home(){
 
@@ -14,14 +16,23 @@ function Home(){
                     page: 1,
                 }
             } )
-            console.log(response.data.results);
+            setFilmes(response.data.results.slice(0, 10));
+            console.log(response.data.results.slice(0, 10));
     }
     loadFilmes();
 }, []);
 
     return(
-        <div>
-            <h1>Bem vindo a home</h1>
+        <div className='container'>
+            {filmes.map((filme) => {
+                return(
+                    <article key={filme.id}>
+                        <strong>{filme.title}</strong>
+                        <img src={`https://image.tmdb.org/t/p/original${filme.poster_path}`} alt={filme.title} />
+                        <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                    </article>
+                )
+            })}
         </div>
     )
 }
